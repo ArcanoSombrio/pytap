68c8a8ad596e529c81afc2f1d137ded5cad51784
bk-201 <bk-201@BK-201.BK-201>
2020-12-09 10:28:34 -0300

    Refactor

--- lib/integrations/ci/pytap_build.bat
+++
@@ -1,5 +0,0 @@
-SET user=xxxxx
-SET password=xxxxxx
-SET url=https://%user%:%password%@gitxxx.com/xxxxx/xxxxx.git
-
-docker build -t pytap . --build-arg git_url=%url%
--- lib/integrations/ci/pytap_build.sh
+++
@@ -1,5 +0,0 @@
-user=xxxxx
-password=xxxxxx
-url=https://$user:$password@gitxxx.com/xxxxx/xxxxx.git
-
-docker build -t pytap . --build-arg git_url=$url
