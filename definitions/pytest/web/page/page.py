617d624097d4cceb3e8140804f25cadbe533b0f3
bk-201 <bk-201@BK-201.BK-201>
2020-12-09 09:00:16 -0300

    Refactor

--- definitions/pytest/web/page/page.py
+++
@@ -1,8 +0,0 @@
-# Exemplo de classe genérica de página para herança de atributos da classe
-class Page(object):
-    url = ''
-    title = ''
-
-    def __init__(self):
-        self.url = self.url
-        self.title = self.title
--- lib/utils/os/get_operational_system.py
+++
@@ -1,6 +0,0 @@
-import platform
-
-
-# Função que retorna o S.O corrente
-def get_operational_system():
-    return platform.system()
